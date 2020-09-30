136f6d30770440497c972b1600c46e949174199a
Qamber Mehdi <qmehdi110@gmail.com>
2020-09-30 02:23:55 -0500

    Login page styling completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Login from './Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Spotify Clone App</h1>
+    <div className="app">
+
+      {/* Spotify Logo */}
+      <Login />
     </div>
   );
 }
