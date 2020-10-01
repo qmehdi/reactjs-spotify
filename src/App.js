f05c7300de7966e44f638d34cca78f6be352a537
Qamber Mehdi <qmehdi110@gmail.com>
2020-09-30 23:40:57 -0500

    Able to get login token back from spotify

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+
+  /* Run code based on a given condition
+     This useEffect will run either when the component first loads and/or
+     whenever the variables inside the '}, [])' change
+  */
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    // The window.location.hash will clear the token so we can do it for security reasons. Commented out for now
+    // window.location.hash = "";
+    console.log(' i have a token >>>', token);
+
+  }, []);
+
   return (
     <div className="app">
 
--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Login.css";
+import { loginUrl } from "./spotify";
 
 function Login() {
   return (
@@ -9,7 +10,7 @@ function Login() {
         alt=""
       />
       {/* <a href={accessUrl}>LOGIN TO SPOTIFY</a> */}
-      <a>LOGIN WITH SPOTIFY</a>
+      <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
     </div>
   );
 }
